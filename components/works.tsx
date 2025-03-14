1efc6ee6e3e9d9e256595be376d4f8628a9c415e
kiannaquines <kjgnaquines@gmail.com>
2025-03-14 15:02:14 +0800

    fix the names

--- components/works.tsx
+++ components/works.tsx
@@ -52,39 +52,39 @@ export default function Works() {
 
 const items = [
     {
-        title: "The Dawn of Innovation",
+        title: "Crowd Monitoring Software",
         description: "Explore the birth of groundbreaking ideas and inventions.",
-        className: "md:col-span-2",
+        className: "md:col-span-1",
         icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
     },
     {
-        title: "The Digital Revolution",
+        title: "Radio Station Log Visualization",
         description: "Dive into the transformative power of technology.",
         className: "md:col-span-1",
         icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
     },
     {
-        title: "The Art of Design",
+        title: "Financial Management System",
         description: "Discover the beauty of thoughtful and functional design.",
         className: "md:col-span-1",
         icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
     },
     {
-        title: "The Power of Communication",
+        title: "Web Application Firewall",
         description:
             "Understand the impact of effective communication in our lives.",
-        className: "md:col-span-2",
+        className: "md:col-span-1",
         icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
     },
     {
-        title: "The Power of Communication",
+        title: "Domain Driven Design Inspired",
         description:
             "Understand the impact of effective communication in our lives.",
-        className: "md:col-span-2",
+        className: "md:col-span-1",
         icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
     },
     {
-        title: "The Art of Design",
+        title: "Artificial Inteligence Chatbot",
         description: "Discover the beauty of thoughtful and functional design.",
         className: "md:col-span-1",
         icon: <IconSignature className="h-4 w-4 text-neutral-500" />,

aee26bda207d53fc1583726916d5e5e9c1f4c146
kiannaquines <kjgnaquines@gmail.com>
2025-03-27 08:51:19 +0800

    Add Vercel Speed Insights integration and update .gitignore for env files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Tektur } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const tektur = Tektur({
   weight: "400",
@@ -32,6 +33,7 @@ export default function RootLayout({
         disableTransitionOnChange
       >
         {children}
+        <SpeedInsights />
       </ThemeProvider>
     </body>
   </html>
